e4a12ffd86f9d95db215ac0449728d0ff84508f1
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-08-03 16:50:32 +0200

    Initial commit

---
+++ src/main/java/com/example/simplepostcollector/SimplePostCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplepostcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePostCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePostCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplepostcollector/SimplePostCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplepostcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePostCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
